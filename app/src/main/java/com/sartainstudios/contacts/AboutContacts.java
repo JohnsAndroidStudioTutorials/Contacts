3133527d2a83453d49ff273ef905584b8fb24f7d
John Sartain <jhnsartain3@gmail.com>
2015-07-25 15:27:53 -0500

    Added comments for understandability. Started implementing Up Navigation

--- app/src/main/java/com/sartainstudios/contacts/AboutContacts.java
+++ app/src/main/java/com/sartainstudios/contacts/AboutContacts.java
@@ -8,19 +8,21 @@ import android.view.MenuItem;
 
 public class AboutContacts extends ActionBarActivity {
 
+    // Starts and shows activity_my_contacts.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_contacts);
     }
 
+    // Adds action bar icons or text to the action bar if present.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_about_contacts, menu);
         return true;
     }
 
+    // Determines if Action bar item was selected. If true then do corresponding action
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
