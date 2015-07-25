324ee19e545c41f7cf5f76818e798d79225e6cc0
John Sartain <jhnsartain3@gmail.com>
2015-07-25 16:26:15 -0500

    Fixed Settings not loading from action bar

--- app/src/main/java/com/sartainstudios/contacts/MyContacts.java
+++ app/src/main/java/com/sartainstudios/contacts/MyContacts.java
@@ -28,12 +28,6 @@ public class MyContacts extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         //handle presses on the action bar items
         switch (item.getItemId()) {
