38c19e7c31c873b41122c2e37a661c81d633d71a
androidstudio <jhnsartain3@gmail.com>
2015-07-26 15:36:13 -0500

    Added imports

--- app/src/main/java/com/sartainstudios/contacts/AboutContacts.java
+++ app/src/main/java/com/sartainstudios/contacts/AboutContacts.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
