f317abfb9ee1b5efbb94693a4ae777050c3a1c0f
John Sartain <jhnsartain3@gmail.com>
2015-07-26 00:06:45 -0500

    Changed AddContact and Search contact to AppCompactActivity Deleted support for up functionality from MyContacts Changed MyContacts image to silhouette Made a string easier to read Add comments for and worked on styles for youtube video Add comments for and worked on styles for youtube video in manifest

--- app/src/main/java/com/sartainstudios/contacts/AddContact.java
+++ app/src/main/java/com/sartainstudios/contacts/AddContact.java
@@ -2,13 +2,13 @@ package com.sartainstudios.contacts;
 
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class AddContact extends ActionBarActivity {
+public class AddContact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
