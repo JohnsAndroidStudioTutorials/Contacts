2ca908fd11e81ce4b00fb0a143c8ae09e7399b2a
John Sartain <jhnsartain3@gmail.com>
2015-08-14 14:55:49 -0500

    Remove comments

--- app/src/main/java/com/sartainstudios/contacts/ContactsSettings.java
+++ app/src/main/java/com/sartainstudios/contacts/ContactsSettings.java
@@ -20,26 +20,9 @@ import android.text.TextUtils;
 
 import java.util.List;
 
-/**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p/>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
- */
 public class ContactsSettings extends PreferenceActivity {
-    /**
-     * Determines whether to always show the simplified settings UI, where
-     * settings are presented in a single list. When false, settings are shown
-     * as a master/detail two-pane view on tablets. When true, a single pane is
-     * shown on tablets.
-     */
-    private static final boolean ALWAYS_SIMPLE_PREFS = false;
 
+    private static final boolean ALWAYS_SIMPLE_PREFS = false;
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -48,11 +31,7 @@ public class ContactsSettings extends PreferenceActivity {
         setupSimplePreferencesScreen();
     }
 
-    /**
-     * Shows the simplified settings UI if the device configuration if the
-     * device configuration dictates that a simplified, single-pane UI should be
-     * shown.
-     */
+
     private void setupSimplePreferencesScreen() {
         if (!isSimplePreferences(this)) {
             return;
