9ce9f208d2d78e22b5a102aba2acfadb1f4beed7
John Sartain <jhnsartain3@gmail.com>
2015-08-15 14:44:59 -0500

    Add Functionality for version display

--- app/src/main/java/com/sartainstudios/contacts/Settings.java
+++ app/src/main/java/com/sartainstudios/contacts/Settings.java
@@ -1,5 +1,6 @@
 package com.sartainstudios.contacts;
 
+import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -12,9 +13,11 @@ import android.preference.ListPreference;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
 import android.preference.PreferenceActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -24,6 +27,48 @@ public class Settings extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        final int versionCode = BuildConfig.VERSION_CODE;
+        final String versionName = BuildConfig.VERSION_NAME;
+        final String ok = getString(R.string.ok);
+        final String thisIsVersionNumber = getString(R.string.this_is_version_number);
+        final String thisIsBuildNumber = getString(R.string.this_is_build_number);
+        final String hereIsWhatsNewInThisVersion = getString(R.string.here_is_whats_new_in_this_version);
+        final String newInThisVersion = getString(R.string.new_in_this_version);
+
+        setVersionNameText();
+
+        TextView versionNameDisplay;
+        versionNameDisplay = (TextView) findViewById(R.id.display_version_name);
+        versionNameDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //display in short period of time
+                Toast.makeText(getApplicationContext(), thisIsVersionNumber + " " + versionCode + " ", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), thisIsBuildNumber + " " + versionName + " ", Toast.LENGTH_SHORT).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
+                builder.setTitle(hereIsWhatsNewInThisVersion)
+                        .setMessage(newInThisVersion)
+                        .setNeutralButton(ok, null); // try to link to version number
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+    }
+
+    private void setVersionNameText() {
+        final String version = getString(R.string.version);
+        String versionName = BuildConfig.VERSION_NAME;
+        TextView displayVersionName = (TextView) findViewById(R.id.display_version_name);
+
+        // sets textView to display version name
+        displayVersionName.setText(version + " " + versionName);
+    }
+}
+
 /*
         final ListPreference listPreference = (ListPreference) findPreference("language");
 
@@ -45,11 +90,11 @@ public class Settings extends PreferenceActivity {
         lp.setEntryValues(entryValues);
     }
 }*/
-        //PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+//PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
 
-        //SharedPreferences getData = Settings.
-        //Boolean;
+//SharedPreferences getData = Settings.
+//Boolean;
         /*
         switch (language()) {
             case R.id.en:
@@ -96,4 +141,3 @@ public class Settings extends PreferenceActivity {
         startActivity(refresh);
     }
     */
-    }}
